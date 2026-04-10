bcec616b703d8e3a65067489e4587dbcd3c0ce5a
foryasith <yasithprashan@gmail.com>
2026-04-10 12:09:52 +0530

    chore: remove frontend client application

--- client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer style={{ padding: "15px", background: "#222", color: "#fff", textAlign: "center" }}>
-      <p>© 2026 MyStore | ERP Integration Demo</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- client/src/pages/Cart.jsx
+++
@@ -1,5 +0,0 @@
-const Cart = () => {
-  return <h1>Cart Page</h1>;
-};
-
-export default Cart;
